9e41a2e51baff34b58bcfd1071d28e95c505ce82
pariya_ab <abadeh.pariya@gmail.com>
2023-11-21 21:36:57 -0500

    plot for the best k is added

--- KNN/Hyperparameter_Tuning/find_the_best_k.py
+++ KNN/Hyperparameter_Tuning/find_the_best_k.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
 
 # Step 1: Read selected features file
 selected_features_df = pd.read_csv("../../Data/Processed_Data/new_selected_features.csv")
@@ -11,6 +11,7 @@ selected_features = selected_features_df['Feature']
 # Read the validation set
 val_df = pd.read_csv("../../Data/Processed_Data/new_val_data.csv")
 val_df = val_df.dropna()
+
 # Extract corresponding features from the validation set
 X_val_selected = val_df[selected_features]
 y_val = val_df['SalePrice']
@@ -24,6 +25,17 @@ for k in k_values:
     scores = cross_val_score(knn, X_val_selected, y_val, cv=5, scoring='neg_mean_squared_error')
     cv_scores.append(-np.mean(scores))
 
+# Find the best K
 best_k = k_values[np.argmin(cv_scores)]
+
+# Plotting
+plt.figure(figsize=(10, 6))  # Adjust figure size
+plt.plot(k_values, cv_scores, marker='o')
+plt.title('Cross-Validation Scores for Different K values')
+plt.xlabel('K (Number of Neighbors)')
+plt.ylabel('Negative Mean Squared Error')
+plt.axvline(x=best_k, color='r', linestyle='--', label=f'Best K: {best_k}')
+plt.legend()
+plt.savefig("best_k.png")
 
 print(f"Best K for KNN: {best_k}")
